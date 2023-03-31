98d69aecd15fc5ac4da920c1ec5b5e7ffd90dfb1
evanknight18 <58967482+evanknight18@users.noreply.github.com>
2023-03-30 20:02:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Get the play button element by its ID
+const playButton = document.getElementById("playButton");
+
+// Create the event listener for the play button click
+playButton.addEventListener("click", function() {
+  // Logged to the console when the play button is clicked
+  console.log("Play button clicked!");
+});
 
