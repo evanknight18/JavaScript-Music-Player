571c1d069081e91baa597b2540a90b8fc8150834
JMHupfer <jmhupfer@gmail.com>
2023-04-08 08:02:11 -0600

    updated ticketmaster API, html container and CSS

--- musicPlayer.js
+++ musicPlayer.js
@@ -1,53 +1,53 @@
-const searchButton = document.querySelector('.search-button');
-const searchInput = document.querySelector('.search-input');
+// const searchButton = document.querySelector('.search-button');
+// const searchInput = document.querySelector('.search-input');
 
-searchButton.addEventListener('click', () => {
-  const query = searchInput.value;
-  searchSongs(query);
-});
+// searchButton.addEventListener('click', () => {
+//   const query = searchInput.value;
+//   searchSongs(query);
+// });
 
 
-  document.addEventListener("DOMContentLoaded", function () {
-    // Select the buttons
-    const previousButton = document.querySelector(".previous");
-    const playButton = document.querySelector(".play");
-    const nextButton = document.querySelector(".next");
+//   document.addEventListener("DOMContentLoaded", function () {
+//     // Select the buttons
+//     const previousButton = document.querySelector(".previous");
+//     const playButton = document.querySelector(".play");
+//     const nextButton = document.querySelector(".next");
   
-    // Event listeners for each button
-    previousButton.addEventListener("click", function () {
-      console.log("Previous button clicked");
-      // Add your functionality for the previous button here
-    });
+//     // Event listeners for each button
+//     previousButton.addEventListener("click", function () {
+//       console.log("Previous button clicked");
+//       // Add your functionality for the previous button here
+//     });
   
-    playButton.addEventListener("click", function () {
-      console.log("Play button clicked");
-      // Add your functionality for the play button here
-    });
+//     playButton.addEventListener("click", function () {
+//       console.log("Play button clicked");
+//       // Add your functionality for the play button here
+//     });
   
-    nextButton.addEventListener("click", function () {
-      console.log("Next button clicked");
-      // Add your functionality for the next button here
-    });
-  });
+//     nextButton.addEventListener("click", function () {
+//       console.log("Next button clicked");
+//       // Add your functionality for the next button here
+//     });
+//   });
   
-  const playButton = document.querySelector(".play");
+//   const playButton = document.querySelector(".play");
 
-playButton.addEventListener("click", () => {
-  const playIcon = playButton.querySelector(".fa-play");
-  const pauseIcon = playButton.querySelector(".fa-pause");
+// playButton.addEventListener("click", () => {
+//   const playIcon = playButton.querySelector(".fa-play");
+//   const pauseIcon = playButton.querySelector(".fa-pause");
 
-  playIcon.style.display = playIcon.style.display === "none" ? "" : "none";
-  pauseIcon.style.display = pauseIcon.style.display === "none" ? "" : "none";
-});
+//   playIcon.style.display = playIcon.style.display === "none" ? "" : "none";
+//   pauseIcon.style.display = pauseIcon.style.display === "none" ? "" : "none";
+// });
 
-playButton.addEventListener('click', function() {
-  if (audio.paused) {
-    audio.play();
-    playButton.classList.remove('fa-play');
-    playButton.classList.add('fa-pause');
-  } else {
-    audio.pause();
-    playButton.classList.remove('fa-pause');
-    playButton.classList.add('fa-play');
-  }
-});
+// playButton.addEventListener('click', function() {
+//   if (audio.paused) {
+//     audio.play();
+//     playButton.classList.remove('fa-play');
+//     playButton.classList.add('fa-pause');
+//   } else {
+//     audio.pause();
+//     playButton.classList.remove('fa-pause');
+//     playButton.classList.add('fa-play');
+//   }
+// });
