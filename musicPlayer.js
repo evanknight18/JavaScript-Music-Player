1d02322877f53e8bd9aa7252f72649fb63960209
Evan Knight <58967482+evanknight18@users.noreply.github.com>
2023-04-08 12:43:02 -0600

    Update musicPlayer.js
    completely updated music player functionality. Load mp3 from local file source button, mp3 data will be displayed in the artist name and song container. Using local storage the mp3 will automatcally save to the playlist container.

--- musicPlayer.js
+++ musicPlayer.js
@@ -1,53 +1,115 @@
-// const searchButton = document.querySelector('.search-button');
-// const searchInput = document.querySelector('.search-input');
+// Initialize variables
+let playlist = [];
+let currentSong = 0;
 
-// searchButton.addEventListener('click', () => {
-//   const query = searchInput.value;
-//   searchSongs(query);
-// });
+// Initialize elements
+const playlistContainer = document.querySelector(".playlist-container");
+const audio = document.querySelector("#audio");
+const albumCoverElement = document.querySelector(".album-cover");
 
 
-//   document.addEventListener("DOMContentLoaded", function () {
-//     // Select the buttons
-//     const previousButton = document.querySelector(".previous");
-//     const playButton = document.querySelector(".play");
-//     const nextButton = document.querySelector(".next");
+// Initialize event listeners
+document.addEventListener("DOMContentLoaded", function() {
+  loadPlaylistFromLocalStorage();
+  displayPlaylist();
+  loadSong(currentSong);
+});
+
+document.querySelector("#audio").addEventListener("ended", function() {
+  nextSong();
+});
+
+document.querySelector("#local-file").addEventListener("change", async function(e) {
+    const files = e.target.files;
+    if (files.length > 0) {
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        if (file.type === "audio/mp3" || file.type === "audio/mpeg") {
+          playlist.push({ title: file.name.split(".")[0], artist: "Local File", file: URL.createObjectURL(file), cover: null });
+        }
+      }
+      displayPlaylist();
+      loadSong(currentSong);
   
-//     // Event listeners for each button
-//     previousButton.addEventListener("click", function () {
-//       console.log("Previous button clicked");
-//       // Add your functionality for the previous button here
-//     });
-  
-//     playButton.addEventListener("click", function () {
-//       console.log("Play button clicked");
-//       // Add your functionality for the play button here
-//     });
-  
-//     nextButton.addEventListener("click", function () {
-//       console.log("Next button clicked");
-//       // Add your functionality for the next button here
-//     });
-//   });
-  
-//   const playButton = document.querySelector(".play");
+      // Save the playlist to local storage
+      localStorage.setItem("playlist", JSON.stringify(playlist));
+    }
+  });
 
-// playButton.addEventListener("click", () => {
-//   const playIcon = playButton.querySelector(".fa-play");
-//   const pauseIcon = playButton.querySelector(".fa-pause");
+playlistContainer.addEventListener("click", function(event) {
+  if (event.target.matches(".song")) {
+    const index = event.target.dataset.index;
+    loadSong(index);
+    playSong();
+  }
+});
 
-//   playIcon.style.display = playIcon.style.display === "none" ? "" : "none";
-//   pauseIcon.style.display = pauseIcon.style.display === "none" ? "" : "none";
-// });
+// Functions
+function loadPlaylistFromLocalStorage() {
+  const storedPlaylist = localStorage.getItem("playlist");
+  if (storedPlaylist) {
+    playlist = JSON.parse(storedPlaylist);
+  }
+}
 
-// playButton.addEventListener('click', function() {
-//   if (audio.paused) {
-//     audio.play();
-//     playButton.classList.remove('fa-play');
-//     playButton.classList.add('fa-pause');
-//   } else {
-//     audio.pause();
-//     playButton.classList.remove('fa-pause');
-//     playButton.classList.add('fa-play');
-//   }
-// });
+function displayPlaylist() {
+  playlistContainer.innerHTML = "";
+  for (let i = 0; i < playlist.length; i++) {
+    const song = playlist[i];
+    const songElement = document.createElement("div");
+    songElement.classList.add("song");
+    songElement.dataset.index = i;
+    if (i === currentSong) {
+      songElement.classList.add("active");
+    }
+    const titleElement = document.createElement("div");
+    titleElement.classList.add("title");
+    titleElement.innerHTML = song.title;
+    const artistElement = document.createElement("div");
+    artistElement.classList.add("artist");
+    artistElement.innerHTML = song.artist;
+    songElement.appendChild(titleElement);
+    songElement.appendChild(artistElement);
+    playlistContainer.appendChild(songElement);
+  }
+}
+
+function loadSong(index) {
+  currentSong = index;
+  const song = playlist[currentSong];
+  audio.src = song.file;
+  const titleElement = document.querySelector(".song-title");
+  titleElement.innerHTML = song.title;
+  const artistElement = document.querySelector(".artist-name");
+  artistElement.innerHTML = song.artist;
+  albumCoverElement.src = song.cover;
+}
+
+function playSong() {
+  audio.play();
+}
+
+function pauseSong() {
+  audio.pause();
+}
+
+function nextSong() {
+  if (currentSong === playlist.length - 1) {
+    currentSong = 0;
+  } else {
+    currentSong++;
+  }
+  loadSong(currentSong);
+  playSong();
+}
+
+function prevSong() {
+  if (currentSong === 0) {
+    currentSong = playlist.length - 1;
+  } else {
+    currentSong--;
+  }
+  loadSong(currentSong);
+  playSong();
+}
+
